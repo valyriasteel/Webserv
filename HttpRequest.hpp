503fac320217a32fe4ed52e3a7d43d6b1f68dcca
Busra Yilmaz <buyilmaz@k2m09s05.42kocaeli.com.tr>
2024-09-09 16:44:54 +0300

    Http methods updated

--- HttpRequest.hpp
+++ HttpRequest.hpp
@@ -5,6 +5,7 @@
 #include <map>
 #include "Server.hpp"
 
+
 class HttpRequest
 {
     private:
@@ -21,11 +22,14 @@ class HttpRequest
         static void handleHttpRequest(int client_fd, const std::string &); 
         static HttpRequest parseHttpRequest(const std::string &);
         void parseHttpRequestUrl();
-        static std::string controlLocationUrl(std::string &, std::vector<Server>&, std::string &);
-        static void handlePostRequest(int client_fd);
-        static void handleDeleteRequest(int client_fd);
+        static std::string controlLocationUrl(const std::string &, std::vector<Server>&);
+        static void handlePostRequest(int client_fd, const std::string& body);
+        static void handleDeleteRequest(int client_fd, const std::string&, const std::string&);
         static void handleUnAllowedRequest(int client_fd);
         static void sendResponse(int client_fd, const std::string& response);
+        static std::string getContentType(const std::string &fileName);
+        static bool isItAllowMethod(const std::string &, std::vector<Server>&, const std::string &);
+        static std::string getRootPath(const std::string &, std::vector<Server>&);
 };
 
 #endif

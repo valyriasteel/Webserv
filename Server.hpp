8f3d3146cc52309b3cfc9c6d8d3d9e0d912341d2
Berat Bosnak <bbosnak@k2m09s07.42kocaeli.com.tr>
2024-09-29 17:21:45 +0300

    default index variable and functions deleted

--- Server.hpp
+++ Server.hpp
@@ -10,7 +10,6 @@ class Server
 		std::string _ip;
 		std::string _serverName;
 		std::string _serverRoot;
-		std::string _serverIndex;
 		int _fd;
 		int _clientMaxBodySize;
 		int _port;
@@ -23,7 +22,6 @@ class Server
 		void setErrorPage(const std::string &, const std::string &, bool);
 		void setName(const std::string &);
 		void setRoot(const std::string &);
-		void setIndex(const std::string &);
 		void setIp(const std::string &);
 		void setPort(int);
 		void setClientMaxBodySize(int);
@@ -40,7 +38,6 @@ class Server
 		static const std::string intToString(int);
 		const std::string &getServerName() const;
 		const std::string &getServerRoot() const;
-		const std::string &getServerIndex() const;
 		const std::string &getIp() const;
 		const std::string &getErrorPath(int);
 		const std::vector<Location> &getLocations() const;
